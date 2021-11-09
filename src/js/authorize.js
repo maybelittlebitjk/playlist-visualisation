2d2e212fdfd1df6e0f477671dd817e971d32d9ec
johk <johkli19@hotmail.se>
2021-11-09 14:26:49 +0100

    all the stuff
    lol

--- src/js/authorize.js
+++ src/js/authorize.js
@@ -1,5 +1,14 @@
-/*
+const loginBtn = document.getElementById("login-button");
 
-  Follow the instructions for "Calling the Spotify playlist API" in the README 
+loginBtn.addEventListener("click", () => {
+  const client_id = process.env.CLIENT_ID;
+  const redirect_uri = 'http://localhost:8080/playlist.html';
 
-*/
+  let url = 'https://accounts.spotify.com/authorize';
+  url += '?response_type=token';
+  url += '&client_id=' + encodeURIComponent(client_id);
+  url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+  
+  window.location.href = url;
+});
+
--- src/js/playlist.js
+++ src/js/playlist.js
@@ -1,6 +1,75 @@
-/*
+function getAccessToken() {
+  const hash = window.location.hash;
+  const hashWithoutHash = hash.substring(1);
 
-  Follow the instructions for "Calling the Spotify playlist API" and
-  "Rendering a playlist visualisation" in the README 
+  const params = hashWithoutHash.split('&');
+  const keyValues = params.map(param => param.split('='));
 
-*/
+  const accessToken = keyValues[0][1];
+  return accessToken;
+};
+
+function getPlaylist(playlist_id) {
+  const url = `https://api.spotify.com/v1/playlists/${playlist_id}`;
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${getAccessToken()}`,
+  };
+  return fetch(url, { headers }).then((response) => response.json());
+};
+
+function renderPlaylist(playlist_id) {
+  const container = document.getElementById('tracks');
+  const audioPlayer = document.getElementById('player');
+  
+
+
+  getPlaylist(playlist_id).then(playlist => {
+    const tracks = playlist.tracks.items;
+
+    for (let i = 0; i < tracks.length; i++) {
+      const track = tracks[i].track;
+
+      const playlistItem = document.createElement('div');
+      playlistItem.classList.add('playlist-item');
+
+      const playlistItemImg = document.createElement('img');
+      playlistItemImg.classList.add('playlist-item-img');
+      playlistItemImg.setAttribute('src', track.album.images[0].url);
+
+      const playlistItemTitle = document.createElement('div');
+      playlistItemTitle.classList.add('playlist-item-title');
+      playlistItemTitle.innerHTML = track.name;
+
+      playlistItem.addEventListener('click', () => {
+          if (currentlyActive === track.id) {
+            audioPlayer.pause();
+            currentlyActive = null;
+            playlistItem.classList.remove('active');
+          } else {
+            if (currentlyActive) {
+              document.querySelector('.active').classList.remove('active');
+            }
+            currentlyActive = track.id;
+            playlistItem.classList.add('active');
+
+            // Play if preview available
+          if (track.preview_url) {
+            audioPlayer.setAttribute('src', track.preview_url);
+            audioPlayer.play();
+          } else {
+            audioPlayer.pause();
+          }
+        }
+      });
+
+      playlistItem.appendChild(playlistItemImg);
+      playlistItem.appendChild(playlistItemTitle);
+      container.appendChild(playlistItem);
+    }
+  });
+};
+
+let currentlyActive;
+renderPlaylist('6zwEB5cOdIATkqx5Fe7sOb');
